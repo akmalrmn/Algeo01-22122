0bac6cfee9e69d729d3d6e11e588f743310aad71
AtqiyaHaydar <ashford1524@gmail.com>
2023-10-02 22:59:40 +0700

    Version 1
    Balikan & SPL Gauss Jordan

--- src/SPL_GaussJordan.java
+++ src/SPL_GaussJordan.java
@@ -1,29 +1,11 @@
+import Matrix.*;
+
 public class SPL_GaussJordan {
     public static void SPLGaussJordan(Matrix matrix) {
         int rows = matrix.getRows();
         int cols = matrix.getCols();
-        Matrix reducedMatrix = new Matrix(rows, cols);
-
-        // Mengisi reducedMatrix dengan data awal matriks
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                reducedMatrix.setElmt(i, j, matrix.getElmt(i, j));
-            }
-        }
-
-        // Membuat Matriks Tereduksi
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (j != i) {
-                    double ratio = reducedMatrix.getElmt(j, i) / reducedMatrix.getElmt(i, i);
-
-                    for (int k = 0; k < cols; k++) {
-                        reducedMatrix.setElmt(j, k, reducedMatrix.getElmt(j, k) - (ratio * reducedMatrix.getElmt(i, k)));
-                    }
-                }
-            }
-        }
-
+        Matrix reducedMatrix = OBE.gaussJordan(matrix);
+    
         // Memeriksa apakah matriks memiliki solusi unik
         boolean hasUniqueSolution = checkUniqueSolution(reducedMatrix);
 
@@ -31,12 +13,8 @@ public class SPL_GaussJordan {
             // Menampilkan hasil dari variabel
             System.out.println("\nHasil SPL:");
             for (int i = 0; i < rows; i++) {
-                System.out.println("Nilai variabel : " + reducedMatrix.getElmt(i, cols - 1) / reducedMatrix.getElmt(i, i));
+                System.out.println("Nilai variabel x-" + (i+1) + ": " + reducedMatrix.getElmt(i, cols - 1) / reducedMatrix.getElmt(i, i));
             }
-
-            // Menampilkan matriks tereduksi ke layar
-            System.out.println("\nMatriks tereduksi: ");
-            reducedMatrix.displayMatrix();
         } else {
             System.out.println("Sistem persamaan tidak memiliki solusi unik.");
         }
@@ -53,18 +31,5 @@ public class SPL_GaussJordan {
         }
 
         return true; // Semua elemen diagonal non-nol, ada solusi unik.
-    }
-
-    // Fungsi menampilkan matriks tereduksi ke layar
-    public static void printReducedMatrix(Matrix matrix) {
-        int rows = matrix.getRows();
-        int cols = matrix.getCols();
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols - 1; j++) {
-                System.out.print(matrix.data[i][j] + "\t");
-            }
-            System.out.println("| " + matrix.data[i][cols - 1]);
-        }
     }
 }
